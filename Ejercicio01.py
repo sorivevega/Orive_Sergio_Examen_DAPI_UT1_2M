c627421a221cc4c9428fc87895c9718f9fd719a6
sorivevega <sorivevega@educacion.navarra.es>
2024-11-15 09:37:30 +0100

    Finalizado Ejercicio Examen 1

---
+++ Ejercicio01.py
@@ -0,0 +1,7 @@
+favorita = input('¿Cual es tu comida favorita? ')
+comida = favorita
+print('Me encanta', comida, 'tambien!')
+
+edad = int(input('¿Cuantos años has vivido? '))
+meses = edad * 12
+print('Has vivido aproximadamente', meses, 'meses')
