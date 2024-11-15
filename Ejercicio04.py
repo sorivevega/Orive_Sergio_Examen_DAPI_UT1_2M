7894c7b1a3a19c1cc39b001cb180aed7ff948fa2
sorivevega <sorivevega@educacion.navarra.es>
2024-11-15 09:38:17 +0100

    Finalizado Ejercicio Examen 4

---
+++ Ejercicio04.py
@@ -0,0 +1,10 @@
+pais = []
+pais = str(input('Escribe 3 paises que te gustaria viajar se parados por comas: '))
+range(pais.split(','))
+for x in range(pais.split(',')):
+    print('Me gustaria visitar', pais[x])
+
+number = []
+number = [(input('Ingrese numeros enteros separados por comas: '))]
+print(max)
+print(min)
