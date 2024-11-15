bea7411948005772f541b96c1f03a7e77a10cb1b
sorivevega <sorivevega@educacion.navarra.es>
2024-11-15 09:38:47 +0100

    Finalizado Ejercicio Examen 5

---
+++ Ejercicio05.py
@@ -0,0 +1,14 @@
+l = float(input('Inserte el lado de su cuadrado: '))
+def area_cuadrado(area):
+    '''
+    En esta funcion lo que haremos sera pedir al usuario el lado de un cuadrado y 
+    con la funcion saldra el area directamente
+
+    Parametros
+
+    l = lado del cuadrado
+    area cuadrado = l * l
+    '''
+    area = l * l
+    return area
+print(area_cuadrado(l))
